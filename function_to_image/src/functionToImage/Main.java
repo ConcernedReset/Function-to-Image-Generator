38706a4dd6abb6f1436d2e63d1db2a3269ce937e
1047588 <1047588@486E303544.lwsd.k12wa.org>
2025-03-20 13:44:33 -0700

    Added Brick Preset

--- function_to_image/src/functionToImage/Main.java
+++ function_to_image/src/functionToImage/Main.java
@@ -12,29 +12,33 @@ public class Main {
 		Picture picture = new Picture(256*3, 256*4);
 		Pixel[][] pixels = picture.getPixels2D();
 		PictureFrame frame = new PictureFrame();
-		double time = 1d;
+		int t = 1;
 		
 		while (true)
 		{
-			time += 0.1;
+			t += 1;
 			for (Pixel[] row : pixels)
 			{
 				for (Pixel p : row)
 				{
 					int x = p.getCol();
 					int y = p.getRow();
+					int mask = 0xFFFFFF;
 					
 					// Noise : (int) (Math.random() * 100)
 					// Deconstructing binary fractal : ((int)((x^y) - time)
 					// 3d Beach : (int)((Math.pow(x, 2) + 90)/(y+time)))
 					// Alien Death : (int)((Math.pow(x, 2) + 90)/(y+time)) * ((int)((x^y) - time)))
 					// Insanity : (int)((Math.pow(x, 2) + 90)/(y+time)) * ((int)((x^y) - time)) * (int)((Math.pow(x, 2) + 90)/(y+time)) * (int)((Math.pow(x, 2) + 90)/(y+time)))
+					// Brick : x^y&t
+					
+					// use & 0xFFFFFF to mask out certian colors, each 2 f's is a color in rgb 
 					
 					p.setColor(new Color(x^y));
+					
 				}
 			}
 			frame.setPicture(picture);
-			
 		}
 	}
 }
